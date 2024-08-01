b74799c177f787eef9049882c89c8b80ac87cead
Erik <erikramirezatl@gmail.com>
2024-08-01 18:13:03 -0400

    added the payment form functionality

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 import { menu } from '/menu.js'
 
+const mainSectionEl = document.getElementById('main-section')
+const menuSectionEl = document.getElementById('menu-section')
 const orderItemsContainerEl = document.getElementById('order-items')
 const totalPriceEl = document.getElementById('total-price')
+const totalSectionEl = document.getElementById('total-section')
+const completeOrderBtn = document.getElementById('complete-order-btn')
+const paymentDetailsForm = document.getElementById('payment-details-form')
+const payBtn = document.getElementById('pay-btn')
 let orderedItems = ``
 let totalPriceCounter = 0
 let amountOrdered = 0
 
+// submits payment form and updates menu html
+payBtn.addEventListener('click', () => {
+    mainSectionEl.innerHTML = `
+        <div class="order-complete">
+            <p>Thanks ERIK, your order is on the way!</p>
+        </div>
+        <button class="return-to-menu-btn" data-reset="" type="button">return to menu</button>
+    `
+
+    console.log('payment sucessfully submitted!')
+})
+
+// reveals payment form 
+completeOrderBtn.addEventListener('click', () => {
+    paymentDetailsForm.classList.remove('hidden')
+
+    console.log('order complete!')
+})
+
+// listens for click to either add or remove item to order
 document.addEventListener('click', (e) => {
         if (e.target.dataset.uuid) {
             addItemToOrder(e.target.dataset.uuid)
@@ -18,6 +44,7 @@ document.addEventListener('click', (e) => {
         
 })
 
+// adds a single item to order and increments count
 function addItemToOrder(itemId) {
     menu.forEach((menuItem) => {
         if (itemId === menuItem.uuid) {
@@ -44,12 +71,13 @@ function addItemToOrder(itemId) {
         }
     })
 
+    totalSectionEl.classList.remove('hidden')
     renderOrder()
     renderTotal()
 }
 
+// removes a single item from the total order and decrements count
 function removeItemFromOrder(e) {
-
     menu.forEach((menuItem) => {
             
         if(menuItem.uuid === e.target.dataset.remove) {
@@ -61,14 +89,20 @@ function removeItemFromOrder(e) {
                 
     })
 
-    console.log(totalPriceCounter, e)
+    if(totalPriceCounter === 0) {
+        totalSectionEl.classList.add('hidden')
+    }
     renderTotal()
 }
 
+// renders the total amount of order
+function renderTotal() {
+    totalPriceEl.innerHTML = '$' + totalPriceCounter
+}
+
+// renders total order
 function renderOrder() {
     orderItemsContainerEl.innerHTML += orderedItems
 }
 
-function renderTotal() {
-    totalPriceEl.innerHTML = '$' + totalPriceCounter
-}
+
